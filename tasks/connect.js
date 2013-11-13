15d6ba49c276a30c0ebf120346043a8a665cd92b
Ólafur Örn Nielsen <olafur@form5.is>
2013-11-13 22:34:41 +0000

    Turn on internal redirect for Angular html5mode

--- tasks/connect.js
+++ tasks/connect.js
@@ -1,9 +1,25 @@
+var modRewrite = require('connect-modrewrite');
+
 module.exports = {
   dev: {
     options: {
       hostname: '0.0.0.0',
       port: 8000,
-      base: 'dev'
-    }
+      base: 'dev',
+      livereload: {
+        options: {
+          open: true,
+          base: 'dev'
+        }
+      },
+      middleware: function (connect, options) {
+        return [
+          modRewrite([
+            '!\\.html|\\.js|\\.css|\\.png$ /index.html [L]'
+          ]),
+          connect.static(require('path').resolve(options.base))
+        ];
+      }
+    },
   }
 };
