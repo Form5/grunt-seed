4b1914cfff12b0792064a968cdd8dbde276886e0
Ólafur Örn Nielsen <olafur@form5.is>
2013-11-13 22:34:58 +0000

    Update bower dependencies

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,11 @@
   require.config({
     baseUrl: 'js',
     paths: {
-      modernizr: 'vendor/modernizr',
-      jQuery: 'vendor/jquery',
-      angular: 'vendor/angular',
-      ngResource: 'vendor/angular-resource',
-      ngRoute: 'vendor/angular-route',
-      ngAnimate: 'vendor/angular-animate',
+      text: 'vendor/text',
+      angular: 'vendor/angular.min',
+      ngResource: 'vendor/angular-resource.min',
+      ngRoute: 'vendor/angular-route.min',
+      ngAnimate: 'vendor/angular-animate.min',
       underscore: 'vendor/underscore'
     },
     shim: {
@@ -15,15 +14,23 @@
       ngResource: { deps: ['angular'] },
       ngRoute: { deps: ['angular'] },
       ngAnimate: { deps: ['angular'] },
-      jQuery: { exports: 'jQuery' },
-      Modernizr: { exports: 'Modernizr' },
       underscore: { exports: '_' }
     }
   });
 
-  require(['jQuery', 'angular'], function($, angular) {
-    $(function() {
+  require(['angular'], function(angular) {
+    var loaded = 0;
+    var bootstrap = function() {
+      if (loaded) return;
+      loaded = 1;
       angular.bootstrap(document, ['app']);
-    });
+    };
+
+    if ( document.readyState === 'complete' ) {
+      setTimeout( bootstrap );
+    } else {
+      document.addEventListener( 'DOMContentLoaded', bootstrap, false );
+      window.addEventListener( 'load', bootstrap, false );
+    }
   });
 }).call(this);
